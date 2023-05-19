e2c2917bf6feb6de589528d75f0b9c56b34b4975
Angel Davila <adavila0703@gmail.com>
2023-05-19 00:24:55 -0500

    Initial files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { FileSelector } from './components/FileSelector'
+import { DataTable } from './components/Table'
+
+function App() {
+  return (
+    <>
+      <h1>GoLens</h1>
+      <FileSelector />
+      <DataTable />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FileSelector.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { Endpoint, get } from '../api'
+
+export const FileSelector: React.FC = () => {
+  return (
+    <div>
+      <div>Select main.go file</div>
+      <input />
+      <button></button>
+    </div>
+  )
+}
