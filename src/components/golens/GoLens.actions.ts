938e4c7f9fc581f62919dd419c7a556df11df744
Angel Davila <adavila0703@gmail.com>
2023-05-23 00:54:29 -0500

    added delete and modal

--- src/components/golens/GoLens.actions.ts
+++ src/components/golens/GoLens.actions.ts
@@ -26,3 +26,29 @@ export const createDirectory =
 export const sortById = createAction('SORT_BY_ID')
 export const sortByName = createAction('SORT_BY_NAME')
 export const sortByCoverage = createAction('SORT_BY_COVERAGE')
+
+export const createDirectoriesLoading = createAction<string>('GET_DATA_TABLE_LOADING')
+export const createDirectoriesFailed = createAction<string>('GET_DATA_TABLE_FAILED')
+export const createDirectoriesCompleted = createAction<any>('GET_DATA_TABLE_COMPLETED')
+
+export const createDirectories =
+  (path: string): AppThunk =>
+  async (dispatch) => {
+    const body = {
+      rootPath: path,
+    }
+    post(body, Endpoint.CreateDirectories).then(() => dispatch(getTableData()))
+  }
+
+export const deleteDirectoryLoading = createAction<string>('GET_DATA_TABLE_LOADING')
+export const deleteDirectoryFailed = createAction<string>('GET_DATA_TABLE_FAILED')
+export const deleteDirectoryCompleted = createAction<any>('GET_DATA_TABLE_COMPLETED')
+
+export const deleteDirectory =
+  (id: string): AppThunk =>
+  async (dispatch) => {
+    const body = {
+      id,
+    }
+    post(body, Endpoint.DeleteDirectory).then(() => dispatch(getTableData()))
+  }
--- src/components/golens/file_selector/FileSelector.tsx
+++ src/components/golens/file_selector/FileSelector.tsx
@@ -1,57 +1,99 @@
 import { useState } from 'react'
 import { useAppDispatch } from '../../../store/store'
-import { createDirectory } from '../GoLens.actions'
+import { createDirectories, createDirectory } from '../GoLens.actions'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import './FileSelector.css'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
+import { TextField } from '@mui/material'
+
+enum TypeSelect {
+  NONE,
+  SINGLE_REPO,
+  MULTI_REPO,
+}
 
 export const FileSelector: React.FC = () => {
-  const [path, setPath] = useState<string>()
   const dispatch = useAppDispatch()
-  const [open, setOpen] = useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
-  const callCreateDirectory = () => {
-    if (path) dispatch(createDirectory(path))
+  const [path, setPath] = useState<string>()
+  const [open, setOpen] = useState(false)
+  const [typeSelect, setTypeSelect] = useState<TypeSelect>(TypeSelect.NONE)
+  const [inputError, setInputError] = useState(false)
+  const [selectError, setSelectError] = useState(false)
+
+  const validateSelections = () => {
+    if (!path || typeSelect == TypeSelect.NONE) {
+      if (!path) {
+        setInputError(true)
+      }
+
+      if (typeSelect == TypeSelect.NONE) {
+        setSelectError(true)
+      }
+
+      return false
+    }
+
+    return true
   }
 
-  const [age, setAge] = useState('')
+  const saveRepo = () => {
+    const validation = validateSelections()
+    if (!validation) {
+      return
+    }
+
+    switch (typeSelect) {
+      case TypeSelect.SINGLE_REPO:
+        if (path) {
+          dispatch(createDirectory(path))
+        }
+        break
+
+      case TypeSelect.MULTI_REPO:
+        if (path) {
+          dispatch(createDirectories(path))
+        }
+        break
+
+      default:
+        break
+    }
+
+    handleClose()
+  }
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string)
+    const selectionType: TypeSelect = Number(event.target.value) as TypeSelect
+    setTypeSelect(selectionType)
   }
 
   return (
     <>
+      <Button
+        className="manage-repo-button"
+        onClick={handleOpen}
+        variant="outlined"
+        style={{ color: 'white', borderColor: 'white' }}
+      >
+        Add Repos
+      </Button>
       <div>
-        <Button
-          className="manage-repo-button"
-          onClick={handleOpen}
-          variant="outlined"
-          style={{ color: 'white', borderColor: 'white' }}
-        >
-          Add Repos
-        </Button>
         <Modal
           open={open}
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box className="file-selector-container" width="100px">
-            <Typography id="modal-modal-title" variant="h5" component="h1">
-              Add Repo
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Select type
-            </Typography>
+          <Box className="file-selector-container">
+            <div className="add-repo-title">Add Repo</div>
             <FormControl fullWidth style={{ backgroundColor: 'white', padding: 1, width: '50%' }}>
               <InputLabel id="demo-simple-select-label" style={{ color: 'black' }}>
                 Type
@@ -59,27 +101,41 @@ export const FileSelector: React.FC = () => {
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={age}
                 label="Type"
                 onChange={handleChange}
                 style={{ color: 'black', borderColor: 'white', backgroundColor: 'white' }}
+                error={selectError}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={TypeSelect.SINGLE_REPO}>Single Repo</MenuItem>
+                <MenuItem value={TypeSelect.MULTI_REPO}>Multi Repo</MenuItem>
               </Select>
             </FormControl>
+            {typeSelect == TypeSelect.MULTI_REPO && (
+              <div className="warning-text">
+                Note: This will walk the given directory and search for any Go projects to add.
+              </div>
+            )}
+            <div className="input-and-buttons">
+              <TextField
+                id="outlined-basic"
+                label="Enter path"
+                variant="outlined"
+                fullWidth
+                onChange={(e) => setPath(e.target.value)}
+                error={inputError}
+              />
+              <Button
+                className="manage-repo-button"
+                onClick={saveRepo}
+                variant="outlined"
+                style={{ color: 'black', borderColor: 'black' }}
+              >
+                Save
+              </Button>
+            </div>
           </Box>
         </Modal>
       </div>
     </>
   )
-
-  // return (
-  //   <div style={{ display: 'flex', flexDirection: 'column' }}>
-  //     <div>Add path</div>
-  //     <input onChange={(e) => setPath(e.target.value)} />
-  //     <button onClick={callCreateDirectory}>Add</button>
-  //   </div>
-  // )
 }
